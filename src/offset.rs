d0cf403982b60ea9de68c7e567edeab37f427625
iagox86 <ron-git@skullsecurity.org>
2020-11-09 13:42:26 -0800

    Add missing offset.rs file

---
+++ src/offset.rs
@@ -0,0 +1,38 @@
+use sized_number::Context;
+
+// Allow us to resolve either statically or dynamically, depending on what's
+// needed. One or the other might throw an error, though.
+#[derive(Debug, Clone, Copy)]
+pub enum Offset<'a> {
+    Static(u64),
+    Dynamic(Context<'a>),
+}
+
+impl<'a> From<u64> for Offset<'a> {
+    fn from(o: u64) -> Offset<'a> {
+        Offset::Static(o)
+    }
+}
+
+impl<'a> From<Context<'a>> for Offset<'a> {
+    fn from(o: Context<'a>) -> Offset<'a> {
+        Offset::Dynamic(o)
+    }
+}
+
+impl<'a> Offset<'a> {
+    pub fn position(&self) -> u64 {
+        match self {
+            Self::Static(n) => *n,
+            Self::Dynamic(c) => c.position(),
+        }
+    }
+
+    pub fn at(&self, offset: u64) -> Offset {
+        match self {
+            Self::Static(_) => Self::Static(offset),
+            Self::Dynamic(c) => Self::Dynamic(c.at(offset)),
+        }
+    }
+}
+
